17fc5a75f3d1c19bb0d89dc7770b7d348b4d95b3
Aaron Smith <aaroncosmith@gmail.com>
2020-01-16 10:24:27 -0500

    more commits im getting good at this

--- pythonDay4/while_loop_3.py
+++ pythonDay4/while_loop_3.py
@@ -1,8 +1,8 @@
 postive = "I hope your having a really great day!"
 
-
+#STOP = 10
 count = 0
 
 while count < len(postive):
-    print(count)
-    count += 1
+    print(postive[count])
+    (count) = count + 1
